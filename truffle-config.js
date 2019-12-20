a443de06c0dce4121acef4b4c2f09a9a012b3faf
rajranjan0608 <rranjan01234@gmail.com>
2019-12-20 15:23:44 +0530

    dotenv

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env.MNEMONICS;
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+process.env.INFURA_ID)
+      },
+      network_id: 3
     }
   },
   solc: {
